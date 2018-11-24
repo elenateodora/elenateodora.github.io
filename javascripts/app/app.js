724a5e7a70cb3cbc22d334dc21ce1dfed316c1f9
Elena Dragne <ella10b@yahoo.com>
2018-11-25 01:04:22 +0200

    first commit

---
+++ javascripts/app/app.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function () {
+    $("#butonModal").click(function () {
+        $("#datas").html($("#lastName").val() + " " +
+            $("#firstName").val() + " " + "from" + " " +
+            $("#address").val() + " " +
+            $("#email").val() + " " +
+            $("#countries").val()
+        )
+    });
+console.log($("#myform").serialize())
+console.log(2+2)
+});
